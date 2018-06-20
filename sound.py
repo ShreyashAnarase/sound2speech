3670d7888c3b15bbe58ca1a334cb4fc7094d4fac
Shreyash <shreyashanarase10@gmail.com>
2018-06-20 23:09:21 +0530

    hi

---
+++ sound.py
@@ -0,0 +1,15 @@
+#converts speech to text
+
+import speech_recognition as sr 
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+	print('Speak')
+	audio = r.listen(source)
+
+	try:
+		text = r.recognize_google(audio)
+		print("You said :{}".format(text))
+	except:
+		print('Sorry couldn\'t understand')
